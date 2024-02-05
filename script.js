711b22df56ce3020a271c0a8cc137babd2b28d1d
leo <leonardo.private@hotmail.com>
2024-02-05 17:35:35 -0300

    fixing img and h1 + some functions

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function encodetext() {
   let originalText = textareaMain.value;
   let codedtext = code(originalText);
   textareaSecondary.value = codedtext;
+  textVerify()
 }
 
 function decodetext() {
@@ -30,9 +31,31 @@ function decodetext() {
   let codedtext = textareaMain.value;
   let decodedtext = decode(codedtext);
   textareaSecondary.value = decodedtext;
+  textVerify()
 }
 
 function copy() {
   let textCopy = document.getElementById("textAreaSecondary");
   navigator.clipboard.writeText(textCopy.value);
+}
+
+function textVerify() {
+  let textCheck = document.getElementById("textAreaSecondary").value;
+  let image = document.getElementById("imagemDiv");
+  let contentPlus = document.getElementById("contentDecode");
+  let sec = document.getElementById("sectionSecondary");
+  let copy = document.getElementById("copy");
+
+  if(textCheck.trim() !== '') {
+      sec.style.justifyContent = "space-between";
+      image.style.display = "none";
+      contentPlus.style.display = "none";
+      copy.style.display = "block";
+  } else {
+    // sec.style.justifyContent = "space-around";
+    image.style.display = "block";
+    contentPlus.style.display = "block";
+    copy.style.display = "none";
+  }
+
 }
